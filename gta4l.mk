e6ba2d892dc30eecd55a4ff13d418083e5495f39
DY3413 <milosix3413@gmail.com>
2024-08-20 19:22:29 +0900

    gta4l-common: Remove folio_daemon
    Signed-off-by: DY3413 <milosix3413@gmail.com>

--- gta4l.mk
+++ gta4l.mk
@@ -168,10 +168,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Folio
-PRODUCT_PACKAGES += \
-    folio_daemon
-
 # FastCharge
 PRODUCT_PACKAGES += \
     vendor.lineage.fastcharge@1.0-service.samsung
